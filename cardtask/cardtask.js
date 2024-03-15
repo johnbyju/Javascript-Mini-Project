737debc95301d821df0dd236d6122ee20a290f83
Johnbyju <138657639+johnbyju@users.noreply.github.com>
2024-03-16 00:33:55 +0530

    Add files via upload

---
+++ cardtask/cardtask.js
@@ -0,0 +1,9 @@
+let empDB ={
+    image : "https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png",
+    empName : "john",
+    empCode : 1412,
+    skills : ["javascript","Front-End","Back-End"],
+}
+
+
+document.getElementById('card-img').src = empDB.image;
